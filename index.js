117be2e44c8d7ca012c27c0970a548ea2e3c3b1a
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-08-19 14:51:05 -0500

    linea para test, codigo hecho en clase y linea de código ternario equivalente. Ambas pasan el test

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path');
+//Misma función más corta (ternaria)
+//module.exports = filePath => path.extname(filePath) === '.md';
+
+//Funcion funcional, pasa los test.
+module.exports = {
+
+  mdLinks : (filePath) =>{
+    if (path.extname(filePath) === '.md'){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  };
+
+
+/*
+  /* saludo :  (name)=>{
+    return `Hola ${name}`;
+  }, */
+/*   mdLinks : () => {
+     return true;
+   },
+*/
+  /* mdLinks : () => {
+     const fs = ('fs');
+     let archivo = fs.readFileSync('/path/to/file') => {
+       return archivo
+   },
+
+});
+
+*/
