f404cd538c09636dd9fc94c7890241d742f29dac
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-08-19 14:52:19 -0500

    Lineas de test escritas en clase con ayuda de Juan y Moy

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,20 @@
-const mdLinks = require('../');
-
+const mdLinks = require("../index.js");
+/*
+describe('mdlinks', () => {
+  it('should be and object', () =>{
+    expect(typeof mdLinks.mdLinks).toBe('object');
+  });
+}
+*/
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return true for a valid .md file', () => {
+    expect( mdLinks.mdLinks('../README.md')).toBe(true);
+  });
+
+  it('should return true for a valid .md file', () => {
+    expect( mdLinks.mdLinks('../README.js')).toBe(false);
   });
 
 });
